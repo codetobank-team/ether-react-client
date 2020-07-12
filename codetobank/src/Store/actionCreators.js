0685b4337bd0b78d62b237a5d5fcac7d47352598
damilola.olawumi <damilola_olawumi@yahoo.com>
2020-07-12 22:04:53 +0100

    added auth action creators

---
+++ codetobank/src/Store/actionCreators.js
@@ -0,0 +1,37 @@
+import * as types from './actionTypes';
+import axios from 'axios';
+import { axiosWithAuth } from '../axiosWithAuth';
+
+export const apiURL = 'https://secret-recipe-be.herokuapp.com/users';
+
+export const register = userData => dispatch => {
+    dispatch({ type: types.REGISTER_START })
+    return axios.post(`${apiURL}/register`, userData)
+        .then(res => {
+            localStorage.setItem('token', res.data.key)
+            dispatch({ type: types.REGISTER_SUCCESS, payload: res.data.key })
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch({ types: types.REGISTER_FAILURE });
+        });
+}
+
+
+export const login = userData => dispatch => {
+    dispatch({ type: types.LOGIN_START })
+    return axios.post(`${apiURL}login`, userData)
+        .then(res => {
+            console.log(res);
+            localStorage.setItem('token', res.data.key);
+            dispatch({ types: types.LOGIN_SUCCESS, payload: res.data.key });
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch({ type: types.LOGIN_FAILURE });
+        });
+}
+
+export const logout = () => {
+    return { type: types.LOGOUT }
+}
