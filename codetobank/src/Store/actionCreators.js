b50378c8c2e83e334f190b6c221136b138fa9f58
damilola.olawumi <damilola_olawumi@yahoo.com>
2020-07-13 17:56:05 +0100

    minor change made

--- codetobank/src/Store/actionCreators.js
+++ codetobank/src/Store/actionCreators.js
@@ -2,32 +2,32 @@ import * as types from './actionTypes';
 import axios from 'axios';
 import { axiosWithAuth } from '../axiosWithAuth';
 
-export const apiURL = 'https://secret-recipe-be.herokuapp.com/users';
+export const apiURL = 'https://cseu3-mud.herokuapp.com/api';
 
 export const register = userData => dispatch => {
     dispatch({ type: types.REGISTER_START })
-    return axios.post(`${apiURL}/register`, userData)
+    return axios.post(`${apiURL}/registration/`, userData)
         .then(res => {
             localStorage.setItem('token', res.data.key)
             dispatch({ type: types.REGISTER_SUCCESS, payload: res.data.key })
         })
         .catch(err => {
-            console.log(err);
-            dispatch({ types: types.REGISTER_FAILURE });
+            // console.log(err);
+            dispatch({ type: types.REGISTER_FAILURE });
         });
 }
 
 
 export const login = userData => dispatch => {
     dispatch({ type: types.LOGIN_START })
-    return axios.post(`${apiURL}login`, userData)
+    return axios.post(`${apiURL}/login/`, userData)
         .then(res => {
-            console.log(res);
+            // console.log(res);
             localStorage.setItem('token', res.data.key);
-            dispatch({ types: types.LOGIN_SUCCESS, payload: res.data.key });
+            dispatch({ type: types.LOGIN_SUCCESS, payload: res.data.key });
         })
         .catch(err => {
-            console.log(err);
+            // console.log(err);
             dispatch({ type: types.LOGIN_FAILURE });
         });
 }
