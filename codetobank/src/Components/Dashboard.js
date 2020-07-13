fec21e902972c7db912d7dae1cf8e53c586679ab
damilola.olawumi <damilola_olawumi@yahoo.com>
2020-07-13 17:56:18 +0100

    wip

--- codetobank/src/Components/Dashboard.js
+++ codetobank/src/Components/Dashboard.js
@@ -4,6 +4,7 @@ function Dashboard (){
     return (
         <div>
             Yooooo this is the Dashboard component...... you must be signed in to see this..Congratulations!!
+            <p></p>
 
         </div>
     )
