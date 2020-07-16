d61f2df0231e2089bbc4de3a7de9137e39d54bb1
damilola.olawumi <damilola_olawumi@yahoo.com>
2020-07-16 03:36:02 +0100

    added styles and created reusable component

---
+++ codetobank/src/Components/CardComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import card from '../Images/card.png';
+
+const Card = (props) => {
+    let user = JSON.parse(localStorage.getItem('user')) || {}
+    return (
+        <div className='dashboard-info' style={{position:'relative'}}>
+            <p className='card-text'>{user.data.firstName} {user.data.lastName}</p>
+            <p className='card-text' style={{
+                bottom:'25px'
+            }}>**** **** **** 2323</p>
+            <img className='card-img' src={card} alt='card' />
+        </div>
+    )
+}
+
+export default Card
