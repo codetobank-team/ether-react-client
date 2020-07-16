dc5129d248346f5cd44f3ed9a9e61c3c14449446
damilola.olawumi <damilola_olawumi@yahoo.com>
2020-07-16 03:36:39 +0100

    included the date

--- codetobank/src/Components/Dashboard.js
+++ codetobank/src/Components/Dashboard.js
@@ -5,7 +5,10 @@ import styled from 'styled-components';
 import Transaction from './Transactions';
 import {DIV}from '../Components/Wallet';
 import {connect} from 'react-redux';
-import {getTransactions} from '../Store/actionCreators';
+import {getTransactions, getWalletDetails} from '../Store/actionCreators';
+import pills from '../Images/pills.png';
+import CardComponent from './CardComponent';
+import moment from 'moment';
 
 
 
@@ -13,15 +16,17 @@ function Dashboard(props) {
 
     useEffect(() => {
         props.getTransactions();
+        props.getWalletDetails()
       }, []);
     
-
+      let user = JSON.parse(localStorage.getItem('user')) || {}
     return (
+      
         <section className='dashboard-section'>
 
             <Header>
                 <P1>Dashboard</P1>
-                <TopBar />
+                <TopBar user={user}/>
             </Header>
 
 
@@ -30,26 +35,27 @@ function Dashboard(props) {
 
                 <section className='dashboard-content-section'>
                     <div className='dashboard-info'>
-                        <P>Available Balance</P>
-                        <p className='date-p'> as at date</p>
-                        <H2>N40,000</H2>
+                    <P>Available Balance </P>
+    <p className='date-p'>as at {moment().format('Do MM, YYYY')}</p>
+    <H2>N{props.walletDetails.balance || "0"}</H2>
                         
 
                         <DIV>
-                            <p>Stay informed : COVID-19</p> <br />
-                            <a href='https://covid19.ncdc.gov.ng/' target='blank'>
-                                Get the latest information from the NSCDC about the COVID-</a>
+                        <img src={pills} style={{width:"22px",height:"22px", marginRight:'9px',marginLeft:'6px'}} />
+                            <div style={{display:"flex", justifyContent:'center', flexDirection:'column'}}>
+                                <p>Stay informed : COVID-19</p> <br />
+                                <a href='https://covid19.ncdc.gov.ng/' target='blank'>
+                                    Get the latest information from the NSCDC about the COVID-</a>
+                            </div>
                         </DIV>
                     </div>
 
-                    <div className='dashboard-info'>
-                        <img className='card-img' src={card} alt='card' />
-                    </div>
+                    <CardComponent />
 
                 </section>
                 <div>
                     <P2>Transaction History</P2>
-                    <Transaction/>
+                    <Transaction transactions = {props.transactions || []}/>
                 </div>
             </div>
 
@@ -57,7 +63,14 @@ function Dashboard(props) {
     )
 }
 
-export default connect(state => state, {getTransactions})(Dashboard);
+const mapStateToProps = (state) => {
+    return {
+        walletDetails: state.transactionReducer.walletDetails,
+        transactions: state.transactionReducer.transactions,
+        user:state.authReducer
+    }
+}
+export default connect(mapStateToProps, {getTransactions,getWalletDetails})(Dashboard);
 
 export const P = styled.p`
 font-family: 'Poppins', sans-serif;
@@ -79,14 +92,18 @@ color: #C41426;
 `;
 
 export const H2 = styled.h2`
-font-family: 'Poppins', sans-serif;
-font-weight: bold;
-font-size: 20px;
-line-height: 50px;
-text-align: center;
-letter-spacing: 1px;
-color: #C41426;;
-text-align: start;
+    font-family: 'Poppins',sans-serif;
+    font-weight: bold;
+    font-size: 40px;
+    margin: 13px 0 28px 0;
+    /* line-height: 50px; */
+    text-align: center;
+    -webkit-letter-spacing: 1px;
+    -moz-letter-spacing: 1px;
+    -ms-letter-spacing: 1px;
+    letter-spacing: 1px;
+    color: #C41426;
+    text-align: start;
 `;
 
 export const Button = styled.button`
@@ -96,15 +113,21 @@ background: #3D8A86;
 border-radius: 8px;
 color:white;
 border: #3D8A86;
+font-weight:500;
+cursor:pointer;
+font-family: 'Poppins',sans-serif;
 `
 export const WhiteButton = styled.button`
 width: 100px;
 height: 42px;
 background: white;
+cursor:pointer;
 border-radius: 8px;
 color:#3D8A86;
 border: 1px solid #3D8A86;
 margin-left:5px;
+font-weight:500;
+font-family: 'Poppins',sans-serif;
 `;
 
 export const Header = styled.div`
--- codetobank/src/Components/Login.js
+++ codetobank/src/Components/Login.js
@@ -62,7 +62,7 @@ function Login(props) {
 
                                 <button className='submit' type='submit'>Login</button>
 
-                                <p className='login-p2'>New to OneBlock?
+                                <p className='bottom-p-text'>New to OneBlock?
                                     <Link to="/"><p style={{color:'red'}}>Sign up here</p>
                                     </Link>
                                 </p>
