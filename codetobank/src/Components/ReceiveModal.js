83a70e59813ea76eb977a5dd1a59675390cc8c90
damilola.olawumi <damilola_olawumi@yahoo.com>
2020-07-16 05:49:35 +0100

    rendered the recieve address to the modal

--- codetobank/src/Components/ReceiveModal.js
+++ codetobank/src/Components/ReceiveModal.js
@@ -1,6 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import qrCode from '../Images/qrCode.png';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
+import { getWalletDetails } from '../Store/actionCreators';
+
+
+function ReceiveModal(props) {
+    useEffect(() => {
+        props.getWalletDetails()
+    }, []);
+
+    const [closeModal, setCloseModal] = useState(false)
+
+    return (
+        <div className='modal'>
+            <div className='form edit-form'>
+                <P1>Recieve Payments</P1>
+                <P2> Scan QR code</P2>
+                <Img src={qrCode} alt='qrCode' />
+
+                <section style={{ display: 'flex' }}>
+                    <Div>{props.walletDetails.address}</Div>
+                    <P3> <i class="fa fa-clone" aria-hidden="true"></i>    Copy </P3>
+                </section>
+
+                <DIv>Go Back</DIv>
+
+
+            </div>
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        walletDetails: state.transactionReducer.walletDetails,
+        user:state.authReducer
+    }
+}
+export default connect(mapStateToProps, { getWalletDetails })(ReceiveModal);
+
 
 const P1 = styled.p`
 color: #C41426;
@@ -37,7 +76,7 @@ background:#FFFFFF;
 height: 50px;
 border: 1px solid rgba(0, 0, 0, 0.15);
 border-radius: 2px 0px 0px 2px;
-font-size: 14px;
+font-size: 10px;
 color: rgba(132, 132, 132, 0.9);
 `;
 
@@ -63,28 +102,3 @@ border-radius: 2px;
 color: #FFFFFF;
 margin-top: 30px;
 `;
-
-function ReceiveModal(props) {
-
-
-    return (
-        <div className='modal'>
-            <div className='form edit-form'>
-                <P1>Recieve Payments</P1>
-                <P2> Scan QR code</P2>
-                <Img src={qrCode} alt='qrCode' />
-
-                <section style={{display:'flex'}}>
-                    <Div>jkhgyjkgjykhjyjk.nkgjyu,k</Div>
-                    <P3>Copy </P3>
-                </section>
-
-                <DIv>Go Back</DIv>
-
-
-            </div>
-        </div>
-    )
-}
-
-export default ReceiveModal;
